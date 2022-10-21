8bf590733229d5ee2a26acaee388bef5710323cd
JasonXChen <jasonxchen888@gmail.com>
2022-10-21 19:24:18 -0400

    Start working on portfolio

---
+++ pages/about.js
@@ -0,0 +1,25 @@
+import styles from "../styles/About.module.css";
+import Image from "next/Image";
+
+export default function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p className={styles.extraGreen}>I Jason. This description.</p>
+            {/* load image locally */}
+            <Image 
+                src="/vercel.svg"
+                alt="vercel logo"
+                width={300}
+                height={300}
+            />
+            {/* load image hosted on another domain */}
+            <Image 
+                src="https://placekitten.com/400/500"
+                alt="portfolio cat"
+                width={400}
+                height={500}
+            />
+        </div>
+    );
+}
