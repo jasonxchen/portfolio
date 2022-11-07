423135484d8ae8cb6cba669c3a0b22c70d66d0d2
JasonXChen <jasonxchen888@gmail.com>
2022-11-07 01:23:24 -0500

    Add live links and fix mobile spacing

--- components/projectList.js
+++ components/projectList.js
@@ -6,14 +6,16 @@ export default function ProjectList() {
             <h2 className="text-4xl mx-auto w-11/12 md:w-10/12">Projects</h2>
 
 			<div className="mt-8 flex flex-wrap justify-center">
-				<Image 
-					src="/img/prediction-example.png"
-					alt="TensorFlow Recycling Example"
-					width={345.5}
-					height={293}
-				/>
-				<div className="w-11/12 md:w-6/12 ml-8">
-					<h3 className="text-3xl mt-2 w-fit">TensorFlow Recycling</h3>
+				<a href="https://github.com/jasonxchen/tensorflow-recycling" target="_blank">
+					<Image 
+						src="/img/prediction-example.png"
+						alt="TensorFlow Recycling Example"
+						width={345.5}
+						height={293}
+					/>
+				</a>
+				<div className="mt-2 w-11/12 md:w-6/12 ml-8">
+					<h3 className="text-3xl w-fit">TensorFlow Recycling</h3>
 					<p>
 						Trained a neural network machine learning model that classifies images of plastic resin recycling codes using TensorFlow, and implemented model utilization on a local web server.
 						Gathered data, built neural networks, trained several models, and made inferences on new data.
@@ -22,14 +24,16 @@ export default function ProjectList() {
 			</div>
 
 			<div className="mt-8 flex flex-wrap justify-center">
-				<Image 
-					src="/img/bloom-home-page.png"
-					alt="Bloom Home Page"
-					width={332.6}
-					height={175.6}
-				/>
-				<div className="w-11/12 md:w-6/12 ml-8">
-					<h3 className="text-3xl mt-2 w-fit">Bloom</h3>
+				<a href="https://bloom-jc.netlify.app" target="_blank">
+					<Image 
+						src="/img/bloom-home-page.png"
+						alt="Bloom Home Page"
+						width={332.6}
+						height={175.6}
+					/>
+				</a>
+				<div className="mt-2 w-11/12 md:w-6/12 ml-8">
+					<h3 className="text-3xl w-fit">Bloom</h3>
 					<p>
 						Built with MERN stack and styled with Tailwind CSS.
 						Full-stack C2C e-commerce web application for buying, selling, updating, and deleting courses/workshops.
@@ -39,14 +43,16 @@ export default function ProjectList() {
 			</div>
 
 			<div className="mt-8 flex flex-wrap justify-center">
-				<Image 
-					src="/img/spotifly-home-page.png"
-					alt="Spotifly Home Page"
-					width={359.75}
-					height={178.5}
-				/>
-				<div className="w-11/12 md:w-6/12 ml-8">
-				<h3 className="text-3xl">Spotifly</h3>
+				<a href="https://spotifly.fly.dev" target="_blank">
+					<Image 
+						src="/img/spotifly-home-page.png"
+						alt="Spotifly Home Page"
+						width={359.75}
+						height={178.5}
+					/>
+				</a>
+				<div className="mt-2 w-11/12 md:w-6/12 ml-8">
+				<h3 className="text-3xl w-fit">Spotifly</h3>
 					<p>
 						Full-stack web application that allows creation of exercise routines using data fetched from an API. 
 						Wrote extensive user authentication and authorization from scratch for requests to the database. 
@@ -56,14 +62,16 @@ export default function ProjectList() {
 			</div>
 
 			<div className="mt-8 flex flex-wrap justify-center">
-				<Image 
-					src="/img/why-so-sus-demo.png"
-					alt="Why So Sus Demo"
-					width={355.75}
-					height={173.5}
-				/>
-				<div className="w-11/12 md:w-6/12 ml-8">
-				<h3 className="text-3xl">Why So Sus?</h3>
+				<a href="https://jasonxchen.github.io/why-so-sus" target="_blank">
+					<Image 
+						src="/img/why-so-sus-demo.png"
+						alt="Why So Sus Demo"
+						width={355.75}
+						height={173.5}
+					/>
+				</a>
+				<div className="mt-2 w-11/12 md:w-6/12 ml-8">
+				<h3 className="text-3xl w-fit">Why So Sus?</h3>
 					<p>
 						Object-oriented single-player desktop browser game with a frame-based rendering method. 
 						Deployed playable product after a week of development using the HTML canvas element for graphics and JavaScript for all game logic.
