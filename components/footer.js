fb193e12c482959db82ae134c59a6e7bc6ed9bc7
JasonXChen <jasonxchen888@gmail.com>
2022-11-07 01:14:10 -0500

    Add Contact section essentials

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,12 @@
 export default function Footer() {
     return (
-        <div id="contact" className="pt-16 h-80">
-            <h2 className="text-4xl text-center">Footer with contact info</h2>
+        <div id="contact" className="pt-20 mt-28 h-80 flex flex-col gap-10">
+            <h2 className="text-4xl text-center">Contact Me</h2>
+            <div className="self-center flex gap-4">
+                <a href="https://github.com/jasonxchen" target="_blank">GitHub</a>
+                <a href="https://www.linkedin.com/in/jasonxchen" target="_blank">LinkedIn</a>
+                <a href="https://docs.google.com/document/d/e/2PACX-1vQx5oaUfUXGcJdrfBNIqwW63VDBrBYY6m-be57P4Vj1VA2uTxWrwJQdvUf9SU7j1Q/pub" target="_blank">Resume</a>
+            </div>
         </div>
     );
 }
